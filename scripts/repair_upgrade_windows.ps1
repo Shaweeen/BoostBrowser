73c3293b97fb8b538c53ba6f2e02490fd9b44c71
chanx <chanx@chanxdeMacBook-Air.local>
2026-08-13 03:39:41 +0800

    release: document cleanup and bump v1.7.102

--- scripts/repair_upgrade_windows.ps1
+++ scripts/repair_upgrade_windows.ps1
@@ -4,7 +4,7 @@
 [CmdletBinding()]
 param(
     [string]$InstallRoot = '',
-    [string]$TargetVersion = 'v1.7.101',
+    [string]$TargetVersion = 'v1.7.102',
     [string]$GitHubOwner = 'Shaweeen',
     [string]$GitHubRepo = 'BoostBrowser',
     [switch]$NoLaunch
