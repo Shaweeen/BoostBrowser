1eda7a43bc235595b2e615a7ebbbdbfa64474e27
chanx <chanx@chanxdeMacBook-Air.local>
2026-08-13 03:28:42 +0800

    release: bump BrowserStudio to v1.7.101

--- scripts/repair_upgrade_windows.ps1
+++ scripts/repair_upgrade_windows.ps1
@@ -4,7 +4,7 @@
 [CmdletBinding()]
 param(
     [string]$InstallRoot = '',
-    [string]$TargetVersion = 'v1.7.100',
+    [string]$TargetVersion = 'v1.7.101',
     [string]$GitHubOwner = 'Shaweeen',
     [string]$GitHubRepo = 'BoostBrowser',
     [switch]$NoLaunch
